1375c57e9c3567be50ff483a1fa0ab37b2f0384f
Loarlam <messiah02@mail.ru>
2020-04-05 15:39:46 +0300

    Add project files.

---
+++ Addition_task/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Используя Visual Studio, создайте проект по шаблону Console Application.  
+Создайте класс MyClass<T>, содержащий статический фабричный метод -  T FacrotyMethod(), 
+который будет порождать экземпляры типа, указанного в качестве параметра типа (указателя места заполнения типом – Т). 
+*/
+using System;
+
+namespace Addition_task
+{
+    class MyClass <T> where T : new()
+    {
+        public static T FactoryMethod()
+        {
+            return new T();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = MyClass<int>.FactoryMethod();
+            Console.WriteLine(a.GetType().Name);
+        }
+    }
+}
